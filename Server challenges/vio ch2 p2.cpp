f4e6445107a668f3ea2c8323e42bc40c9ae80712
Sami Bouchelouche <bouchlouchsami525@gmail.com>
2024-11-25 00:16:17 +0100

    new program

--- Server challenges/vio ch2 p2.cpp
+++ Server challenges/vio ch2 p2.cpp
@@ -3,15 +3,27 @@
 using namespace std;
 int main()
 {
-    int N;
-    int number[N];
-    cout << "Enter N : ";
-    cin >> N;
-    for (int i = 1; i <= N; i++)
-    {
-        cin >> number[N];
-    }
-    cout << number[N];
+    int N, i;
+    int number[N] ;
 
+    cout<< "Enter a natural number : ";
+    cin>> N;
+    cin>> number[N];
+    if (N < 0)
+    {
+        cout<<"The number must be positive."<<endl;
+        return 0;
+    }
+    i = 1;
+    while (i*i <= number[N])
+    {
+        if (i*i == number[N])
+        {
+            cout<<number[N]<<" is a perfect square. Its square root is "<<i<<"."<< endl;
+            return 0;
+        }
+        i=i+1;
+    }
+    cout<< number[N] <<" is not a perfect square."<< endl;
     return 0;
 }
